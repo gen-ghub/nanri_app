1a0bb5af380a5c7ccbc3d7b48b8e74039d413a4d
gen-ghub <renbenzhuang234@gmail.com>
2020-12-26 11:17:43 +0900

    スライド導入準備

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,10 +18,12 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    'swiper/dist/css/swiper.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~plugins/vue-awesome-swiper',
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
